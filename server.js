d227b52d2312a9b5a5bc8f252d00b0ad6f62b024
Onesuch <onesuch07@gmail.com>
2018-10-28 15:14:56 +0300

    change server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+/* Установить предварительно nodemon -g */
 'use strict';
 const express = require('express'), // наше приложение с использованием express
     app = express(), // запуск main страницы
     mongoose = require('mongoose'), // драйвер для подключения к mongodb
+    path = require('path'), // дает возможность бегать по папкам
     logger = require('morgan'), // отслеживание ошибок
     bodyParser = require('body-parser'), // парсим в json запросы корректно
     cookieParser = require('cookie-parser'), // Парсер куки
@@ -31,6 +33,29 @@ app.use(bodyParser.urlencoded({extended: true}));
 /* По этому маршруту делаем запросы */
 app.use(['/reg'], registration);
 /* ------------------ */
+
+/* Поиск проекта и содержимого */
+app.get('/', function(req, res) {
+    res.sendfile('dist/index.html');
+});
+
+app.use('/', express.static(__dirname + '/dist'));
+/*app.use('/game-table', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/html/game-table/game-table.html'));
+    res.render('html/game-table/game-table.html', {title: 'Игровые новости'});
+});*/
+/* --------------------------- */
+
+/* error 404 */
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + '/client/html/404/404.html');
+});
+
+/* error 500 */
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Что-то сломалось!');
+});
 
 /* Запуск сервера по порту */
 app.listen(3000, () => {
